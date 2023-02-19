c2121d5326a8d9a7fd6fca76ee8be9a5e53bc2c5
Akshay Jadhav <akshayjadhav2133@gmail.com>
2023-02-19 11:03:45 +0530

    use state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 
+import { useState } from 'react';
 import './App.css';
+
+function Counter(){
+   
+  const [count,setcount]=useState(10);
+
+  function addone(){
+    setcount(count+1);
+
+  }
+  
+  
+  return <>
+        <button onClick={addone} >
+          Click 
+        </button>
+        <p>{count}</p>
+  
+  </>
+}
 
 function App() {
   return (
     <div className="App">
+      
       <h1>Hi APJ</h1>
+      <Counter/>
+      <Counter/>
+      <Counter/>
+      <Counter/>
+      <Counter/>
     </div>
   );
 }
